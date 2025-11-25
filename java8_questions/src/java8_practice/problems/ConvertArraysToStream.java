7767b7760f831471ee9b3464f1ffd9061824565b
Ankit Srivastava <44730520+ankitsrivastava07@users.noreply.github.com>
2025-11-25 13:58:53 +0530

    Remove unnecessary blank line in convertArraysTOStream

--- java8_questions/src/java8_practice/problems/ConvertArraysToStream.java
+++ java8_questions/src/java8_practice/problems/ConvertArraysToStream.java
@@ -7,7 +7,6 @@ public class ConvertArraysToStream {
 	public void convertArraysTOStream(int ars[]) {
 		Stream<Integer> stream = Arrays.stream(ars);
 		Integer arrsLength = stream.count();
-
 		
 		System.out.println(arrsLength);
 	}
