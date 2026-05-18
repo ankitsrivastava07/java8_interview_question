fcbfb1f8b7c7b67107a437b8fa85b783c6bc0305
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2026-05-18 18:55:57 +0530

    code changes adding

--- java8_questions/src/java8_practice/problems/MoveZerosToRightPlaceProblem.java
+++ java8_questions/src/java8_practice/problems/MoveZerosToRightPlaceProblem.java
@@ -1,10 +1,23 @@
 package java8_practice.problems;
 
-public class RemoveZerosToRightPlaceProblem {
+import java.util.Arrays;
 
-    public
+public class MoveZerosToRightPlaceProblem {
+
+    public static int[] moveZeroToRightPlace(int[] numbs) {
+        int ind = 0;
+        for (int i = 0; i < numbs.length; i++) {
+            if (numbs[i] != 0)
+                numbs[ind++] = numbs[i];
+        }
+        while (ind < numbs.length) {
+            numbs[ind++] = 0;
+        }
+        return numbs;
+    }
 
     public static void main(String[] args) {
-
+        int[] numbs = {0, 5, 0, 8, 6, 0, 1, 9};
+        System.out.println(Arrays.toString(moveZeroToRightPlace(numbs)));
     }
 }
