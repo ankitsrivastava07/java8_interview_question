556cf8631f08ccd5a927b16820a00583f3e3ff28
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2026-05-18 18:55:44 +0530

    code changes adding

--- java8_questions/src/java8_practice/problems/LargestStringProblem.java
+++ java8_questions/src/java8_practice/problems/LargestStringProblem.java
@@ -1,4 +1,25 @@
 package java8_practice.problems;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class LargestStringProblem {
+
+    public static String findOrderLargestLengthString(String str, final int order) {
+        String[] words = str.split("\\s+");
+        return Arrays
+                .stream(words)
+                .sorted(Comparator
+                        .comparingInt(String::length)
+                        .reversed())
+                .skip(order)
+                .findFirst()
+                .orElse("");
+    }
+
+    public static void main(String[] args) {
+        String str = "I am Ankit Srivastava";
+        String result = findOrderLargestLengthString(str, 3);
+        System.out.println(result);
+    }
 }
